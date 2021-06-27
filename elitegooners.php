52dc3a38e48939bc7679d8b6f7315f8b700058bc
YoungShittu <39669395+YoungShittu@users.noreply.github.com>
2021-06-28 00:28:21 +0100

    Add files via upload

---
+++ elitegooners.php
@@ -0,0 +1,164 @@
+<?php
+echo "hello world";
+echo "<br/>";
+echo 2 + 2;
+?>
+
+
+<!DOCTYPE HTML> 
+<html lang="en"> 
+    <head> 
+        <meta charset="utf‑8"> 
+        <!‑‑ Responsive ‑‑> 
+        <meta name="viewport" content="width=device‑width, initial‑scale=1"> 
+        <!‑‑ CSS ‑‑> 
+        <!‑‑W3 CSS‑‑> 
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+        <!‑‑Font Awesome‑‑> 
+        <!‑‑Online Address‑‑> 
+        <script src="https://kit.fontawesome.com/5468eb592b.js" crossorigin="anonymous"></script> 
+        <!‑‑Local Address‑‑> 
+        <link href="fontawesome/css/all.css" rel="stylesheet"/> 
+        <!‑‑Google Font‑‑>
+        <!‑‑Online Address‑‑> 
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster"> 
+        <!‑‑Custom CSS‑‑> 
+        <link id="" rel="stylesheet" type="text/css" media="screen" href="style.css" /> 
+        <style> </style> 
+        <title>Elite Gooners</title> 
+    </head> 
+    <body> 
+        
+        <!‑‑ Content ‑‑> 
+        <div class="container">
+        <header>
+            <h1>
+                <i class="fas fa-trophy"></i>
+                ELITE GOONERS
+                <i class="fas fa-trophy"></i>
+            </h1>
+        </header>
+
+        <!--MEMBERS LIST-->
+        <section class="Members">
+            <table id="table1">
+                <caption>Elite Members</caption>
+                <tr>
+                    <th>S/N</th>
+                    <th>
+                        Names
+                        <i class="fab far fa-user"></i>
+                    </th>
+                    <th>
+                        Post
+                        <i class="far fas fa-award"
+                    </th>
+                    <th>
+                        Country
+                        <i class="far fa-flag"
+                    </th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Lolade</td>
+                    <td>Admin 
+                        <i class="fas fa-crown"></i>
+                        <i class="fas fa-user-check"></i>
+                    </td>
+                    <td>Nigeria</td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>HarryPee</td>
+                    <td>Admin 
+                        <i class="fas fa-crown"></i>
+                    <td>Nigeria</td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Kingsley</td>
+                    <td>Admin 
+                        <i class="fas fa-crown"></i>
+                    <td>Rwanda</td>
+                </tr>
+                <tr>
+                    <td>4.</td>
+                    <td>Samuel Ologun</td>
+                    <td>Admin 
+                        <i class="fas fa-crown"></i></td>
+                    <td>Nigeria</td>
+                </tr>
+                <tr>
+                    <td>5.</td>
+                    <td>Zamani</td>
+                    <td>Member 
+                        <i class="fas fa-medal"></i>
+                    </td>
+                    <td>Nigeria</td>
+                </tr>  
+                <tr>
+                    <td>6.</td>
+                    <td>Mastersharo</td>
+                    <td>Member
+                        <i class="fas fa-medal"></i>
+                    </td>
+                    <td>Tanzania</td>
+                </tr>              
+                <tr>
+                    <td>7.</td>
+                    <td>MR REX</td>
+                    <td>Member
+                        <i class="fas fa-medal"></i>
+                    </td>
+                    <td>Nigeria</td>
+                </tr>  
+
+                <tfoot>
+                    <tr>
+                        <td colspan="4">
+                            <i class="fas fa-hammer"></i>
+                            Underconstruction 
+                            <i class="fas fa-paint-roller"></i> 
+                            <i class="fas fa-cog fa-spin"></i>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+            <table id="table2">
+                            <!--ICONS DESCRIPTION-->              
+                <tr>
+                    <td class="iconsRole">
+                        <i class="fas fa-user-check"></i>
+                            <span>- Owner/Creator</span>
+                    </td>
+                    <td class="iconsRole">
+                        <i class="fas fa-crown"></i>
+                            <span>- Admin/Founding Member</span>                        
+                    </td>
+                </tr>
+                <tr>
+                    <td class="iconsRole">
+                        <i class="fas fa-medal"></i>
+                            <span>- Founding Member</span>
+                    </td>
+                    <!--<td class="iconsRole" colspan="2">
+                        <i class="fas fa-user-check"></i>
+                            <span>- Owner/Creator</span>  
+                    </td>-->
+                </tr>
+            </table>
+
+        </section>
+
+        <footer>
+            <h4>Arsenal FC...simply for the Elites.</h4>
+        </footer>
+
+        </div>
+
+
+        <script></script> 
+
+    </body> 
+</html>
+
