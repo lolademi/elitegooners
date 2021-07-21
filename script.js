b4f3b523d924e124fad1436cafccbd91541947df
YoungShittu <39669395+YoungShittu@users.noreply.github.com>
2021-07-21 02:05:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,34 +31,54 @@ window.onclick = function(event) {
 
 // Constructor function for Person objects
 class Member {
-  constructor(name, country, post, eye) {
+  constructor(name, country, post, icons) {
     this.name = name;
     this.country = country;
     this.post = post;
-    this.eyeColor = eye;
+    this.icons = icons;
   }
 }
 
   
 // Create Members object
-var lolade = new Member("Lolade", "Nigeria", `Admin` +  '<i class="fas fa-crown"></i> <i class="fas fa-user-check"></i>', "black");
-var harrypee = new Member("HarryPee", "Nigeria", `Admin` +  '<i class="fas fa-crown">', "black");
-var kingsley = new Member("Kingsley", "Rwanda", `Admin` +  '<i class="fas fa-crown"></i>', "black");
-var samuel = new Member("Lolade", "Nigeria", `Admin` +  '<i class="fas fa-crown"></i>', "black");
-var zamani = new Member("Lolade", "Nigeria", `Member` +  '<i class="fas fa-crown"></i>', "black");
+var lolade = new Member("Lolade", "Nigeria", `Admin`, '<i class="fas fa-medal"></i> <i class="fas fa-crown"></i> <i class="fas fa-user-check"></i>');
+var harrypee = new Member("HarryPee", "Nigeria", `Admin`, '<i class="fas fa-medal"></i> <i class="fas fa-crown">');
+var kingsley = new Member("Kingsley", "Rwanda", `Admin`, '<i class="fas fa-medal"></i> <i class="fas fa-crown"></i>');
+var samuel = new Member("Sam Ologun", "Nigeria", `Admin`,'<i class="fas fa-medal"></i> <i class="fas fa-crown"></i>');
+var zamani = new Member("Zamani", "Nigeria", `Member`, '<i class="fas fa-medal"></i>');
 
 // Display properties
-document.getElementById("m1").innerHTML = lolade.name; 
-document.getElementById("m1-ctry").innerHTML = lolade.country;
-document.getElementById("m1-post").innerHTML = lolade.post;
-document.getElementById("m2").innerHTML = harrypee.name; 
-document.getElementById("m2-ctry").innerHTML = harrypee.country;
-document.getElementById("m2-post").innerHTML = harrypee.post;
-document.getElementById("m3").innerHTML = kingsley.name; 
-document.getElementById("m3-ctry").innerHTML = kingsley.country;
-document.getElementById("m3-post").innerHTML = kingsley.post;
 
+//lolade
+document.getElementById("M1-NAME").innerHTML = lolade.name; 
+document.getElementById("M1-CTRY").innerHTML = lolade.country;
+document.getElementById("M1-POST").innerHTML = lolade.post;
+document.getElementById("M1-ICONS").innerHTML = lolade.icons;
+//harrypee
+document.getElementById("M2-NAME").innerHTML = harrypee.name; 
+document.getElementById("M2-CTRY").innerHTML = harrypee.country;
+document.getElementById("M2-POST").innerHTML = harrypee.post;
+document.getElementById("M2-ICONS").innerHTML = harrypee.icons;
+//kingsley
+document.getElementById("M3-NAME").innerHTML = kingsley.name; 
+document.getElementById("M3-CTRY").innerHTML = kingsley.country;
+document.getElementById("M3-POST").innerHTML = kingsley.post;
+document.getElementById("M3-ICONS").innerHTML = kingsley.icons;
+//samuel
+document.getElementById("M4-NAME").innerHTML = samuel.name; 
+document.getElementById("M4-CTRY").innerHTML = samuel.country;
+document.getElementById("M4-POST").innerHTML = samuel.post;
+document.getElementById("M4-ICONS").innerHTML = samuel.icons;
+//zamani
+document.getElementById("M5-NAME").innerHTML = zamani.name; 
+document.getElementById("M5-CTRY").innerHTML = zamani.country;
+document.getElementById("M5-POST").innerHTML = zamani.post;
+document.getElementById("M5-ICONS").innerHTML = zamani.icons;
 
+document.getElementById("M6-NAME").innerHTML = mastersharo.name; 
+document.getElementById("M6-CTRY").innerHTML = mastersharo.country;
+document.getElementById("M6-POST").innerHTML = mastersharo.post;
+document.getElementById("M6-ICONS").innerHTML = mastersharo.icons;
 
 //Creating an array of members
 //Members = [lolade, harrypee, kingsley];
