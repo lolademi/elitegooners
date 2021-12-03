48c1c430a4bd1a093eb8c0fdb676e4ed80973cbc
YoungShittu <39669395+YoungShittu@users.noreply.github.com>
2021-12-04 00:17:05 +0100

    Update reactJS.jsx

--- reactJS.jsx
+++ reactJS.jsx
@@ -3,35 +3,33 @@
 class Hello extends React.Component {
     render() {
       return (
-          <div class="row">
-          <div class="square">
-            <div class="img">
-              {this.props.img}
-            </div>
+        <div class="column">
+        <div class="square"><div class="img">{this.props.img}</div></div>
+        <div class="details">
+          <div class="text-center">
+            <h3 id="M1-NAME" class="font-mono text-2xl">{this.props.name}</h3>
+            <p id="M1-ICONS" class="flex justify-around text-lg">
+              {this.props.icons}
+            </p>
           </div>
-          <div class="details">
-            <div class="text-center">
-              <h3 id="M1-NAME" class="font-mono text-2xl"> 
-                {this.props.name} 
-              </h3>
-              <p id="M1-ICONS" class="flex justify-around text-lg"> 
-                {this.props.icons} 
-              </p>
-            </div>
-              <br />
-            <div class="text-left text-base">
-              <span>ID: </span>
-                <p id="M1-ID" class="inline"> {this.props.id} </p><br/>
-              <span>Country: </span>
-                <p id="M1-CTRY" class="inline"> {this.props.country} </p><br/>
-              <span>Post: </span>
-                <p id="M1-POST" class="inline"> {this.props.post} </p><br/>
-              <span>Fav. Player: </span>
-                <p id="M1-FAVPLAYER" class="inline"> {this.props.fplayer} </p><br/>
-            </div>
-            
+          <br />
+          <div class="text-left text-base">
+            <span>ID: </span>
+            <p class="inline">{this.props.id}</p>
+            <br />
+            <span>Country: </span>
+            <p class="inline">{this.props.country}</p>
+            <br />
+            <span>Post: </span>
+            <p class="inline">{this.props.post}</p>
+            <br />
+            <span>Fav. Player: </span>
+            <p class="inline">{this.props.fplayer}</p>
+            <br />
           </div>
         </div>
+      </div>
+      
       );
     }
   }
@@ -94,8 +92,8 @@ class Hello extends React.Component {
                 fplayer="Granit Xhaka"
                 icons={[<i class="fas fa-medal"></i>]}
                 /> 
-                <Hello name="Mayowa" id="06" 
-                img ={[<img src='images/zamani.jpg'/>]}
+                <Hello name="Mayowa" id="" 
+                img ={[<img src='images/profile1.jpeg'/>]}
                 country="Nigeria" 
                 post="Elite Member" 
                 fplayer="Granit Xhaka"
@@ -115,12 +113,19 @@ class Hello extends React.Component {
                 fplayer=""
                 icons={[<i class="fas fa-medal"></i>]}
                 />
-                <Hello name="Mary" id="06" 
+                <Hello name="Godfrey" id="" 
+                img ={[<img src='images/profile1.jpeg'/>]}
+                country="Kenya" 
+                post="Member" 
+                fplayer="Unknown"
+                icons={[<i class="fas fa-user"></i>]}
+                />
+                <Hello name="Mary" id="" 
                 img ={[<img src='images/profile6.png'/>]}
                 country="Nigeria" 
-                post="Elite Member" 
+                post="Member" 
                 fplayer="Unknown"
-                icons={[<i class="fas fa-medal"></i>]}
+                icons={[<i class="fas fa-user"></i>]}
                 />       
           </div>
       );
