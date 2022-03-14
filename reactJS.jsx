ebb1821ef36f3652e748f2748d669727cb143ecd
YoungShittu <39669395+YoungShittu@users.noreply.github.com>
2022-03-14 13:14:42 +0100

    update reactJS.jsx

--- reactJS.jsx
+++ reactJS.jsx
@@ -3,7 +3,7 @@ class Hello extends React.Component {
     return (
       <div class="column flex w-full   p-4 md:w-96 min-w-fit">
         <div class="square mx-auto shadow-xl border-solid rounded-full">
-          <div class="profile_pic">{this.props.img}</div>
+          <div class="profile_pic rounded-full">{this.props.img}</div>
         </div>
 
         <div class="details ml-2 p-1 leading-loose">
